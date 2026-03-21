771e08f6a9203aa177a66a6502a3833106c6ee3a
Fabian <fabian@Fabians-MBP.fritz.box>
2026-03-21 16:02:21 +0100

    Add analytics

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { mount } from 'svelte';
 import App from './App.svelte';
 import './app.css';
 import { injectSpeedInsights } from '@vercel/speed-insights';
+import { inject } from '@vercel/analytics';
 
+inject();
 injectSpeedInsights();
 
 const app = mount(App, { target: document.getElementById('app') });
