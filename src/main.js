eb32e6724e2796dd27d0e7a84db65f3ffcffd8c7
Fabian <fabian@Fabians-MBP.fritz.box>
2026-03-21 14:35:58 +0100

    Add vercel insigths

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { mount } from 'svelte';
 import App from './App.svelte';
 import './app.css';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
+injectSpeedInsights();
 
 const app = mount(App, { target: document.getElementById('app') });
 
